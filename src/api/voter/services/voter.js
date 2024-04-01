1fb55dd610fb300cac9d1ea1a1df7cdff121d9ce
Abduljawad AbuAlrous <abduljawad.abualrous@kw.zain.com>
2024-04-01 03:00:01 +0300

    test package

--- src/api/voter/services/voter.js
+++ src/api/voter/services/voter.js
@@ -8,7 +8,7 @@ const { createCoreService } = require('@strapi/strapi').factories;
 
 module.exports = createCoreService('api::voter.voter', ({ strapi }) => ({
   async totalVotersService(query) {
-    const getData = await strapi.entityService.findMany('api::voter.voter', query);
-    return getData.length;
+    const getData = await strapi.entityService.count('api::voter.voter', query);
+    return getData;
   },
 }));
